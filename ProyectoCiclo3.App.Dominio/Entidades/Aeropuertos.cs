c9cb4e7a70b75ad5b882674c7b0c7ce280860ddb
Your Name <you@example.com>
2021-09-22 07:09:27 -0500

    comitt v1

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aeropuerto {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+        public int coord_x { get; set; }
+        public int coord_y { get; set; }
+        
+    }
+ 
+}
+
---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aviones{
+        public int id { get; set; }
+        public string modelo { get; set; }
+        public int numero_banos { get; set; }
+        public int numero_asientos { get; set; }
+        public int capacidad_max_carga { get; set; }
+        
+    }
+ 
+}
